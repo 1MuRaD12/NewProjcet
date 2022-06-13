8841994787186048fc0f63aa887baee325706d7b
1MuRaD12 <tu71tfsws@code.edu.az>
2022-06-13 11:01:08 +0300

    first commit

---
+++ backend project/project/project/Controllers/DetailsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace project.Controllers
+{
+    public class DetailsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ backend project/project/project/ViewModels/HomeVM.cs
@@ -0,0 +1,12 @@
+﻿using project.Models;
+using System.Collections.Generic;
+
+namespace project.ViewModels
+{
+    public class HomeVM
+    {
+        public List<Details> details { get; set; }
+
+        public List<Information> Information { get; set; }
+    }
+}
